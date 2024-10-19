cccb8f07f63bf83dd437df11245b1b44943b6171
Malevyn <darrenjames.publico@icloud.com>
2024-10-19 22:32:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Fast Script For Friends
+A script that auto add attack speed to your characters (Currently Priest Supported) 
+
+## Usage & Command 
+Type "fast" in /8 to enable or disable
+Type "fast reload in /8 when you adjust skill.js in the file  
+
+## Supported Skills 
+SP/ NGSP / PR / SHEN NGSP
+
+## NOTICE 
+This is a personal script ( DO NOT SHARE )
---
+++ skills.js
@@ -0,0 +1,11 @@
+//priest
+module.exports.priest = [
+	{ "id": 38, "delay": 0, "speed": 5 }, // Backstep
+    { "id": 26, "delay": 0, "speed": 10 }, // Escape
+	{ "id": 31, "delay": 0, "speed": 150 },// Guardian
+    { "id": 19, "delay": 0, "speed": 5 }, // Focus Heal
+	{ "id": 12, "delay": 0, "speed": 5 }, // Resurrect
+	{ "id": 11, "delay": 0, "speed": 20 }, // Metamorphic Blast
+	{ "id": 16, "delay": 0, "speed": 20 }, // Shocking Implosion
+	{ "id": 27, "delay": 0, "speed": 20 }, // Final Reprisal
+];
